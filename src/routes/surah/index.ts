2fcaeac3347dad8d935a77d283bade8e972aca8e
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-05-07 07:16:03 +0700

    feat(api): get surah by id (unstable)

--- src/routes/surah/index.ts
+++ src/routes/surah/index.ts
@@ -1,9 +1,21 @@
 import { Hono } from "hono"
-import alfatihah from '../../../data/surah/1-alfatihah'
+import alfatihah from '../../../data/surah/1'
 const surah = new Hono().basePath('/surah')
 
 surah.get('/alfatihah',  (c) => {
     return c.json(alfatihah)
   })
+
+surah.get('/:id', (c) => {
+
+  const id = c.req.param('id')
+  const surahFile = id + '.ts'
+  const surahData = require('../../../data/surah/' + surahFile)
+  return c.json({
+    status: 200,
+    message: 'Success to get surah data',
+    data: surahData
+  })
+})
 
 export default surah
